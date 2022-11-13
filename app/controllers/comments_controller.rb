9a991f9092b7bc829063582bbe1f584fc39e0c04
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-13 16:34:45 -0500

    create method in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,14 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
 
+    if @comment.save
+      redirect_to @commentable
+    else
+      redirect_to @commentable, alert: 'Something went wrong'
+    end
   end
 
   private
