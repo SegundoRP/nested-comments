cc0c7629a17d20ed01e030a9e93d764ee4c6faf4
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-12-07 00:32:05 -0500

    added kaminari gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
-    @comments = @post.comments.where(parent_id: nil).page(params[:page])
+    @comments = @post.comments.where(parent_id: nil).page(params[:page]).per(5)
   end
 
   # GET /posts/new
